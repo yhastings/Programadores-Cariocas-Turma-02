7fc484e4b8dda73ef3a1bde9670fb47a5a14579f
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-06 14:53:10 -0300

    Atividade desenvolvida com os participantes programadoras cariocas

---
+++ Javascript/index.js
@@ -0,0 +1,19 @@
+// let nome, sonhos, dinheiro
+// // alert(nome)
+// // console.log(nome)
+
+// nome = prompt("Escreva o seu nome");
+// // alert("Seu nome é "  + nome);
+// dinheiro = prompt("Seu dinheiro");
+// alert(nome + "\n" + dinheiro);
+
+
+let nome, idade, gostoMusical, filmeFavorito, filhos, sonhos
+nome = prompt("Olá qual é o seu nome?");
+idade = prompt("E qual é a sua idade");
+gostoMusical = prompt("Fiquei curiosa qual seu estilo musical?");
+filmeFavorito = prompt("Massa! mas qual é o seu filme favorito?");
+filhos = prompt("E você tem filhos?");
+sonhos = prompt("Você é incrivel! pra finalizar qual é seu maior sonho?");
+
+alert("Aqui está um resumo sobre " + nome + "\nTem " + idade + " anos" + "\nAma ouvir " + gostoMusical + "\nE o filme favorito é " + filmeFavorito + "\nTem " + filhos + " filhos" + "\nE seu maior sonho é, " + sonhos )
