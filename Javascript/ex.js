f241df76b72867385db6b33762dd2e35ee8e9acd
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-12 10:00:44 -0300

    Resolução exercicios logicos

---
+++ Javascript/ex.js
@@ -0,0 +1,30 @@
+// 1 - Faça um Algoritmo que receba o preço de custo de um produto e mostre o valor da venda. sabe-se que o preço de custo receberá um acréscimo de acordo com um percentual informado pelo usuário 
+// let precoCusto, porcentual, porcentagem;
+// alert("Seja bem vindo(a) vendedor(a), posso calcular?");
+// precoCusto = prompt("Qual o valor do produto?");
+// porcentual = prompt("Qual o porcentual adicional?");
+// porcentagem = (precoCusto / 100) * porcentual;
+// alert(parseFloat(precoCusto) + parseFloat(porcentagem));
+
+//2 - faça um algoritmo que receba um valor que foi depositado e exiba o valor com rendimento após um mês. considere fixo o juro da poupança em 0,70% a.m
+// let deposito, taxa, rendimento
+// deposito = prompt("Olá aqui é o banco de todos, qual o valor do seu deposito?");
+// taxa = 0.70
+// rendimento = parseFloat(deposito) * (taxa / 100);
+// alert(parseFloat(deposito) + parseFloat(rendimento));
+
+//3 - Faça um algoritmo que efetua a apresentação do valor da conversão em real (R$) de um valor lido em dólar (US$). o algoritmo deverá solicitar o valor da cotação do dólar.
+// let dolar, cotacao
+// dolar = prompt("Qual é a quantidade de dolar U$");
+// cotacao = prompt("Qual é a cotação?");
+// alert("Valor em R$: " + (parseFloat(dolar)) * (parseFloat(cotacao)));
+
+// 4 - Escrever um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 80, menor que 25 ou igual a 40
+// let numero 
+// numero = prompt("Qual é o seu numero?");
+// if(parseFloat(numero) > 80 || parseFloat(numero) < 25 || parseFloat(numero) == 40){
+//     alert("Você conseguiu!");
+// }else{
+//     alert("Tente na proxima");
+// }
+
