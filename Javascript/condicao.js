062ddaf5b608cf36dc8a2156171882219e842598
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-12 10:35:25 -0300

    Exemplos de condição

---
+++ Javascript/condicao.js
@@ -0,0 +1,25 @@
+// let idade;
+
+// idade = prompt("Qual é sua idade");
+
+// if(idade <= 15){
+//     alert("Já pode planejar a festa de 15 anos");
+// }else if(idade >= 18){
+//     alert("Já pode sair de casa");
+// }else{
+//     alert("Tá quase lá");
+// }
+
+
+// let aldeia
+// aldeia = prompt("Qual aldeia você precisa destruir" + "\nFolha" + "\nAreia" + "\nNevoa" + "\nSom");
+// switch(aldeia){
+//     case "folha": alert("Os ninjas dela são Naruto, Kakashi, Sasuke, Sakura, Ino, Tsunade, Rocklee");
+//     break;
+//     case "Areia": alert("Os ninjas dela são  Gaara, Temari, Kankuro, Sasori");
+//     break;
+//     case "Nevoa": alert("Zabuza, Haku, Kisame,Suigetsu");
+//     break;
+//     case "Som": alert("Orochimaru, Kimimaru, Kabuto, Log");
+//     break;
+// }
