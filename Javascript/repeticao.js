a4a46537f3ffe9d8ce1f11c853dd5b6dab1bc3ae
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-13 14:24:59 -0300

    Aula sobre laços de repetição

---
+++ Javascript/repeticao.js
@@ -0,0 +1,41 @@
+// for   // para
+// while  // enquanto
+// do-while //faça enquanto 
+
+
+// for(inicialização; condição; incremento/decremento){
+    // bloco ação
+// }
+// alert("Sua bomba vai explodir em: ");
+// for(i = 0; i < 10; i++){
+//     alert(i);
+// }
+// alert("Explodiu!!!!");
+
+// let abacaxi = 0; // inicialização
+// while(abacaxi < 5){ //condição
+//     for(i = 0; i < 4; i++){
+//         alert(i);
+//     }
+//     abacaxi++
+//     alert("Sou While");
+      
+    // alert(abacaxi)
+// }
+
+
+  //inicialização
+  //condição
+  //incremento
+
+//     let n = 0; //inicialização
+//   while(n < x){ //condição 
+//         alert(n)
+//         n + 1 //incremento 
+//   } 
+
+// let i = 0;
+// do{
+//     alert(i);
+//     i++;
+// }while(i < 5);
