f525c19b71aaa2e25f30246b55e7868bc42251f5
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-15 11:54:44 -0300

    Exemplos em sala de função

---
+++ Javascript/function.js
@@ -0,0 +1,24 @@
+// let n1, n2, resultado
+// n1 = prompt("Digite um numero: ");
+// n2 = prompt("Digite o segundo numero: ");
+
+// function abacaxi(n1, n2){
+
+//    resultado = parseInt(n1) + parseInt(n2) 
+//     return alert(resultado);
+// }
+
+// abacaxi(n1, n2);
+
+
+// do {
+//     opcao = MostrarMenu();
+// }while(opcao != 2)
+
+// function MostrarMenu(){
+//     entrada = "";
+//     alert("==-Mostrar o Menu-==" + 
+//     "\n1 - Mostrar o menu" + 
+//     "\n2 - Sair");
+//     return entrada = prompt();
+// }
