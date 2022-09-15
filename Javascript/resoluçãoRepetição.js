df2dcc5bf92ffb8f89328c7edadec75acb477401
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-15 11:46:39 -0300

    Resolução dos exercicios de laço de repetição

---
+++ Javascript/resoluçãoRepetição.js
@@ -0,0 +1,93 @@
+// 1 - 
+
+// let filhos, sal100 = 0, salario, soma = 0, mediaSal = 0.0, mediaF = 0, mSal = 0.0, perc
+// for(i = 0; i <= 3; i++){
+//     salario = prompt("Digite seu salário");
+//     filhos = prompt("Digite o numero de filhos");
+
+//     soma = parseFloat(soma) + parseFloat(salario);
+//     mediaF = parseInt(mediaF) + parseInt(filhos);
+
+//     if(mSal < salario){
+//         mSal = parseFloat(salario);
+//     }
+
+//     if(salario <= 100 ){
+//         parseFloat(sal100++);
+//     }
+// }
+
+// mediaSal = soma / 4;
+// mediaF = parseInt(mediaF) / 4;
+// perc = parseFloat(sal100) / 4 * 100;
+
+// alert("A media do salário da populção: " + mediaSal.toFixed(2) + "\nMedia de filhos da população é: " + mediaF + "\nO maior salario foi: " + mSal + "\nO Percentual de pessoa com salario até 100 é: " + perc + "%");
+
+// 2 
+
+// let num = 0.0;
+
+// for(i = 0; i <= 500; i++){
+//     if(i % 2 != 0 && i % 3 == 0){
+//         alert(num);
+//         num = num + i 
+//     }
+
+// }
+
+// alert("A soma de todos os numeros impares e divisiveis por 3\nde 1 a 500 é: " + num);
+
+// let num = 0.0, soma = 0.0, media, total = 0.0;
+
+//     while(num >= 0){
+//         num = prompt("Digite o numero: ");
+
+//         if(num >= 0){
+//             soma = parseInt(soma) + parseInt(num)
+//             total++ 
+//         }
+//     }
+
+//     media = soma / total;
+
+//     alert("Soma de todos numeros digitados: " + soma);
+//     alert("Media de todos numeros digitados: " + media);
+//     alert("Total de numeros digitados: " + total);
+    
+
+// 4 
+
+// let num = 0;
+//     num = prompt("Digite um numero: ");
+
+//     while(num <= 100){
+//         num = num * 3
+//         alert(num);
+//     }
+
+
+// 5 
+
+// let cont = 233;
+
+//     do{
+//         if(cont >= 300 && cont <= 400){
+//             cont += 3;
+//         }else{
+//             cont += 5;
+//         }
+//         alert(cont);
+
+//     }while(cont <= 456);
+
+// 5 
+
+// let num = 0, resultado = 0;
+
+// num = prompt("Digite um numero: ");
+
+// do{
+//     resultado = parseInt(resultado) + parseInt(num);
+//     num--
+
+// }while(num >= 0 );
