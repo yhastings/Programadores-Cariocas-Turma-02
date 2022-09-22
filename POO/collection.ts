bbe3c5cd7d4b47978c65b40cc9b0f5bcc42a9e87
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-09-22 15:32:22 -0300

    Exemplo de Collections

---
+++ POO/collection.ts
@@ -0,0 +1,52 @@
+//Forma de inicializar nosso array
+
+// let identificador = new Array<string>
+// let identificador: string[] = []
+// let identificador = ["Nome", "Joana"]
+
+let alunos: string[] = []
+
+
+//populando um array 
+alunos.push('Debora', 'Romulo', 'Mayara', 'Sergio');
+// alunos.push()
+
+
+
+// console.log(alunos[2]);
+
+// for(let i = 0; i <= 3; i++){
+//     console.log(alunos[i])
+// }
+
+// for( let i of alunos){
+//     console.log(i)
+// }
+
+
+// alunos.forEach((abacaxi) => console.log(abacaxi))
+
+//remove o ultimo elemento
+
+// console.log(alunos);
+
+// alunos.pop()
+// alunos.pop()
+
+// Remove o primeiro elemento do array 
+// console.log(alunos);
+
+// alunos.shift()
+
+
+// console.log(alunos);
+
+
+//alterar ou remover dentro do array
+
+console.log(alunos);
+
+// alunos.splice(1, 1)
+alunos.splice(2, 3, "Fernanda", "Tharles")
+
+console.log(alunos);
