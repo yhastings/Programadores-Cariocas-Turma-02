7a0cf191f7a04013937d5a84b0e314dc25fa2ae7
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-11-29 13:03:37 -0300

    Exemplo de refatoração do codigo

---
+++ React/hello-world/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+
+function App() {
+  return (
+      <Router>
+        <Navbar />
+          <Routes>
+              <Route path='/' element={<Login />}/>
+              <Route path='/home' element={<Home />}/>
+              <Route path='/cadastrousuario' element={<CadastroUsuario />}/>
+          </Routes>
+        <Footer /> 
+      </Router>
+  );
+}
+
+export default App;
---
+++ React/hello-world/src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+
+
+function Navbar() {
+    return (
+
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor' >
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
+
+export default Navbar;
